ea8e66d05752568d3c9a75ca45a195a9b21b5945
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-23 22:01:45 +0800

    Compiling for google cloud app engine

--- cmd/prometheus/scripts/appdeploy.sh
+++ cmd/prometheus/scripts/appdeploy.sh
@@ -12,14 +12,33 @@
 export GCLOUD_PROJECT=
 . ../../.env
 
+OLD_GOPATH=${GOPATH}
+export GOPATH=${OLD_GOPATH}/src/github.com/prometheus/prometheus/vendor
+
 echo GOROOT=${GOROOT}
 echo GOPATH=${GOPATH}
+echo OLD_GOPATH=${OLD_GOPATH}
+
+echo ========= ${name} ========= mkdir -p ${GOPATH}/github.com/prometheus
+mkdir -p ${GOPATH}/github.com/prometheus
+
+echo ========= ${name} ========= ln -s ${OLD_GOPATH}/src/github.com/prometheus/prometheus ${GOPATH}/github.com/prometheus/prometheus
+ln -s ${OLD_GOPATH}/src/github.com/prometheus/prometheus ${GOPATH}/github.com/prometheus/prometheus
+
+echo ========= ${name} ========= ln -s ${GOPATH} ${GOPATH}/src
+ln -s ${GOPATH} ${GOPATH}/src
 
 # Deploy to Google Cloud.
+echo ========= ${name} ========= gcloud config set project ${GCLOUD_PROJECT}
 gcloud config set project ${GCLOUD_PROJECT}
+
+echo ========= ${name} ========= gcloud config list project
 gcloud config list project
 
 echo ========= ${name} ========= gcloud app deploy app.yaml --quiet --verbosity=info
 gcloud app deploy app.yaml --quiet --verbosity=info
 
 echo ========= ${name} Deployed! =========
+
+export GOPATH=${OLD_GOPATH}
+echo GOPATH=${GOPATH}
